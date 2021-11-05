0f2fedada92ef1a076a67feda2de6fb4257d7b47
Saulo Daniel da Silva <Saulo.Silva-ext@ab-Inbev.com>
2021-11-05 12:55:21 -0300

    My first provisionament unto spotfy by terraform with new musics

--- main.tf
+++ main.tf
@@ -9,12 +9,35 @@ terraform {
 provider "spotify" {
   api_key = var.spotify_api_key
 }
-data "spotify_search_track" "by_artist" {
-  artist = "Dolly Parton"
-  album = "Jolene"
-  name  = "Early Morning Breeze"
+
+data "spotify_track" "IveGotaWoman" {
+  url = "https://open.spotify.com/track/2xar08Fq5xra2KKZs5Bw9j?si=a560a4d67eb742d0"
 }
+data "spotify_track" "HallelujahILoveHerSo" {
+  url = "https://open.spotify.com/track/5P11rW6aJErF37MTfRZS31?si=8d58c0317cab406b"
+}
+data "spotify_track" "Ederlezi" {
+  url = "https://open.spotify.com/track/6J3FOMuB6XYlAxaZxH4sdT?si=b814d7003d584b68"
+}
+
 resource "spotify_playlist" "playlist" {
+  name        = "My playlist Terraform"
+  description = "My playlist is so awesome"
+  public      = false
+
+  tracks = [
+    data.spotify_track.IveGotaWoman.id,
+    data.spotify_track.HallelujahILoveHerSo.id,
+    data.spotify_track.Ederlezi.id,
+  ]
+}
+
+/* data "spotify_search_track" "by_artist" {
+  artist = "Dolly Parton"
+  album = "Jolene"  
+ # name  = "Early Morning Breeze"
+} */
+/* resource "spotify_search_track" "playlist" {
   name        = "Playlist automatized by Terraform"
   description = "This playlist was created by Terraform"
   public      = true
@@ -24,4 +47,4 @@ resource "spotify_playlist" "playlist" {
     data.spotify_search_track.by_artist.tracks[1].id,
     data.spotify_search_track.by_artist.tracks[2].id,
   ]
-}
+} */
